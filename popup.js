a4afba230fba4b755601ce31e19eb580a56ec37e
Alexander Balson <alexander.balson@gmail.com>
2015-09-10 23:07:08 +1000

    Option page, background page files added, manifest changed accordingly

--- popup.js
+++ popup.js
@@ -1,30 +1,31 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var checkPageButton = document.getElementById('checkPage');
-  checkPageButton.addEventListener('click', function() {
+document.addEventListener('DOMContentLoaded', function() 
+{
+	var checkPageButton = document.getElementById('checkPage');
+	checkPageButton.addEventListener('click', function() 
+	{
+		chrome.tabs.executeScript
+		(
+			{
+				code: 'document.body.style.backgroundColor="red"'
+				//To inject code from code in a file:
+				//chrome.tabs.executeScript(null, {file: "content_script.js"});
+			}
+		);
 
-    console.log("turn it all red");
-
-    chrome.tabs.executeScript
-    (
-      {
-        code: 'document.body.style.backgroundColor="red"'
-      }
-    );
-
-    //chrome.tabs.getSelected(null, function(tab) 
-    //{
-      // d = document;
-
-      // var f = d.createElement('form');
-      // f.action = 'http://gtmetrix.com/analyze.html?bm';
-      // f.method = 'post';
-      // var i = d.createElement('input');
-      // i.type = 'hidden';
-      // i.name = 'url';
-      // i.value = tab.url;
-      // f.appendChild(i);
-      // d.body.appendChild(f);
-      // f.submit();
-    //});
-  }, false);
+		//From the tutorial; how to use url in logic:
+		//chrome.tabs.getSelected(null, function(tab) 
+		//{
+			// d = document;
+			// var f = d.createElement('form');
+			// f.action = 'http://gtmetrix.com/analyze.html?bm';
+			// f.method = 'post';
+			// var i = d.createElement('input');
+			// i.type = 'hidden';
+			// i.name = 'url';
+			// i.value = tab.url;
+			// f.appendChild(i);
+			// d.body.appendChild(f);
+			// f.submit();
+		//});
+	}, false);
 }, false);
